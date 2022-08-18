f450f660e01b4f252b7228815cd23a48745e7c53
cutlets14 <cutlets14@gmail.com>
2022-08-17 20:03:20 -0400

    More practice with input; simpler structure

--- ex12.py
+++ ex12.py
@@ -1,1 +1,7 @@
 # Prompting people
+# Rewriting the previous exercise by using the prompt as an argument to input()
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("So you're %r old, %r tall and %r heavy." % (age, height, weight))
